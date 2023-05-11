16a5859d4d930bdcc9bcfe2092e64ee248e7b91d
DeepanshuChugh <deepanshuchugh24@gmail.com>
2023-05-11 10:40:12 +0530

    Initial Commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped lang="scss">
+.q-page-container {
+  background: rgb(10, 10, 0);
+  background: linear-gradient(135deg, rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 0.7) 50%, rgba(100, 100, 97, 0.7) 100%);
+}
+</style>
